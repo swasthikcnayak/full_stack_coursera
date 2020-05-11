7d16d5fe704d74a0158be996ecba6e85fa731081
swasthikcnayak <swasthik0304@gmail.com>
2020-05-11 15:35:38 +0530

    react redux form:

--- c2/react/confusion/src/components/ContactComponent.js
+++ c2/react/confusion/src/components/ContactComponent.js
@@ -7,8 +7,9 @@ import {
   Input,
   Col,
   Button,
-  FormFeedback,
+  FormFeedback,Row
 } from "reactstrap";
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from "react-router-dom";
 import { render } from "@testing-library/react";
 
@@ -16,85 +17,21 @@ class Contact extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      firstname: "",
-      lastname: "",
-      telnum: "",
-      email: "",
-      agree: false,
-      contactType: "Tel.",
-      message: "",
-      touched: {
-        firstname: false,
-        lastname: false,
-        telnum: false,
-        emial: false,
-      },
-    };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(event) {
-    console.log("Current State is: " + JSON.stringify(this.state));
-    alert("Current State is: " + JSON.stringify(this.state));
-    event.preventDefault();
-  }
-
-  handleBlur = (field) => (event) => {
-    this.setState({ touched: { ...this.state.touched, [field]: true } });
-  };
-
-  validate(firstname, lastname, telnum, email) {
-    const errors = {
-      firstname: "",
-      lastname: "",
-      telnum: "",
-      email: "",
-    };
-
-    if (this.state.touched.firstname && firstname.length < 3)
-      errors.firstname = "First Name should be > 3 characters";
-    else if (firstname.length > 10)
-      errors.firstname = "First Name should be < 10 charactes";
-
-    if (this.state.touched.lastname && lastname.length < 3)
-      errors.lastname = "Last Name should be > 3 characters";
-    else if (lastname.length > 10)
-      errors.lastname = "Last Name should be < 10 charactes";
-
-    const reg = /^\d+$/;
-
-    if (this.state.touched.telnum && !reg.test(telnum))
-      errors.telnum = "Tel. Number should contain only numbers";
-
-    if (
-      this.state.touched.emial &&
-      email.split("").filter((x) => x === "@").length != 1
-    )
-      errors.email = "Email should contain a @ sign";
-
-    return errors;
+  handleSubmit(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
   }
 
   render() {
-    const errors = this.validate(
-      this.state.firstname,
-      this.state.lastname,
-      this.state.telnum,
-      this.state.email
-    );
+    // const errors = this.validate(
+    //   this.state.firstname,
+    //   this.state.lastname,
+    //   this.state.telnum,
+    //   this.state.email
+    // );
     return (
       <div className="container">
         <div className="row">
@@ -160,7 +97,120 @@ class Contact extends Component {
             <h3>Send us Your Feedback</h3>
           </div>
           <div className="col-12 com-md-9">
-            <form onSubmit={this.handleSubmit}>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
+                <Label htmlFor="firstname" md={2}>
+                  First Name
+                </Label>
+                <Col md={10}>
+                  <Control.text
+                    model=".firstname"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="First Name"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="lastname" md={2}>
+                  Last Name
+                </Label>
+                <Col md={10}>
+                  <Control.text
+                    model=".lastname"
+                    id="lastname"
+                    name="lastname"
+                    placeholder="Last Name"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="telnum" md={2}>
+                  Contact Tel.
+                </Label>
+                <Col md={10}>
+                  <Control.text
+                    model=".telnum"
+                    id="telnum"
+                    name="telnum"
+                    placeholder="Tel. Number"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="email" md={2}>
+                  Email
+                </Label>
+                <Col md={10}>
+                  <Control.text
+                    model=".email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col md={{ size: 6, offset: 2 }}>
+                  <div className="form-check">
+                    <Label check>
+                      <Control.checkbox
+                        model=".agree"
+                        name="agree"
+                        className="form-check-input"
+                      />{" "}
+                      <strong>May we contact you?</strong>
+                    </Label>
+                  </div>
+                </Col>
+                <Col md={{ size: 3, offset: 1 }}>
+                  <Control.select
+                    model=".contactType"
+                    name="contactType"
+                    className="form-control"
+                  >
+                    <option>Tel.</option>
+                    <option>Email</option>
+                  </Control.select>
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="message" md={2}>
+                  Your Feedback
+                </Label>
+                <Col md={10}>
+                  <Control.textarea
+                    model=".message"
+                    id="message"
+                    name="message"
+                    rows="12"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type="submit" color="primary">
+                    Send Feedback
+                  </Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
+
+{
+  /* <Form onSubmit={this.handleSubmit}>
               <FormGroup row>
                 <Label htmlFor="firstname" md={2}>
                   First Name
@@ -285,12 +335,70 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </FormGroup>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+            </Form> */
 }
 
-export default Contact;
+// handleBlur = (field) => (event) => {
+//   this.setState({ touched: { ...this.state.touched, [field]: true } });
+// };
+
+// validate(firstname, lastname, telnum, email) {
+//   const errors = {
+//     firstname: "",
+//     lastname: "",
+//     telnum: "",
+//     email: "",
+//   };
+
+//   if (this.state.touched.firstname && firstname.length < 3)
+//     errors.firstname = "First Name should be > 3 characters";
+//   else if (firstname.length > 10)
+//     errors.firstname = "First Name should be < 10 charactes";
+
+//   if (this.state.touched.lastname && lastname.length < 3)
+//     errors.lastname = "Last Name should be > 3 characters";
+//   else if (lastname.length > 10)
+//     errors.lastname = "Last Name should be < 10 charactes";
+
+//   const reg = /^\d+$/;
+
+//   if (this.state.touched.telnum && !reg.test(telnum))
+//     errors.telnum = "Tel. Number should contain only numbers";
+
+//   if (
+//     this.state.touched.emial &&
+//     email.split("").filter((x) => x === "@").length != 1
+//   )
+//     errors.email = "Email should contain a @ sign";
+
+//   return errors;
+// }
+
+// handleInputChange(event) {
+//   const target = event.target;
+//   const value = target.type === "checkbox" ? target.checked : target.value;
+//   const name = target.name;
+
+//   this.setState({
+//     [name]: value,
+//   });
+// }
+
+// this.state = {
+//   firstname: "",
+//   lastname: "",
+//   telnum: "",
+//   email: "",
+//   agree: false,
+//   contactType: "Tel.",
+//   message: "",
+//   touched: {
+//     firstname: false,
+//     lastname: false,
+//     telnum: false,
+//     emial: false,
+//   },
+// };
+
+//this.handleInputChange = this.handleInputChange.bind(this);
+// this.handleBlur = this.handleBlur.bind(this);
