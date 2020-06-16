6668ce07c6ca2ae255cb9f6fc6c305739ba4b340
swasthikcnayak <swasthik0304@gmail.com>
2020-06-17 01:33:02 +0530

    morgan and express added

--- node/node-main/index.js
+++ node/node-main/index.js
@@ -1,55 +1,18 @@
-const http = require("http");
-const hostname = "localhost";
-const port = "3000";
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
 
-const server = http.createServer((req,res) =>
-{
-    if(req.method=='GET')
-    {
-        var fileurl;
-        if(req.url == '/')
-            fileurl = '/index.html'
-        else
-            fileurl = req.url;
-        var filepath = path.resolve('./public')+fileurl;
-        var fileext = path.extname(filepath);
-        if(fileext == '.html')
-        {
-            fs.exists(filepath,(exists)=>
-            {
-                if(!exists)
-                {
-                    res.statusCode='404';
-                    res.setHeader('content-type','text/html');
-                    res.end('<html><body><h1>The desired file is not found </h1></body></html>')
-                
-                }
-                else
-                {
-                    res.statusCode = '200';
-                    res.setHeader('content-type','text/html');
-                    fs.createReadStream(filepath).pipe(res);
-                }
-            })
-        }
-        else
-        {
-            res.statusCode='404';
-            res.setHeader('content-type','text/html');
-            res.end('<html><body><h1>Only html files can be served</h1></body></html>')
-     
-        }
-    }
-    else
-    {
-        res.statusCode='404';
-        res.setHeader('content-type','text/html');
-        res.end('<html><body><h1>Only GET request can be served</h1></body></html>')
-    }
-})
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-  console.log(`Server is setup and running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
