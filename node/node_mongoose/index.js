6ef705dd1d40e3744a18d553569f972a643b80c6
swasthikcnayak <swasthik0304@gmail.com>
2020-06-19 15:28:27 +0530

    mongoose 2

--- node/node_mongoose/index.js
+++ node/node_mongoose/index.js
@@ -6,31 +6,25 @@ const url = 'mongodb://localhost:27017/conFusion';
 
 const connect = mongoose.connect(url);
 
-connect.then((db)=>
-{
+connect.then((db) => {
     console.log('connected to the server');
 
-    var newDish = Dishes({
-        name : "Uthapizza",
+    Dishes.create({
+        name: "Uthapizza",
         description: "Testing"
     })
-    newDish.save()
-    .then((dish) => {
-        console.log("Inserting a dish")
-        console.log(dish);
-        return Dishes.find({}).exec();
-    })
-    .then((dishes)=>
-    {
-        console.log("printing all dishes")
-        console.log(dishes);
-        return Dishes.remove({});
-    })
-    .then(()=>
-    {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then((dish) => {
+            console.log(dish);
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 })
